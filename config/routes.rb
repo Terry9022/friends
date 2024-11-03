5c6ec7fb09a2ab2d24a462be80a1ae098eb25ad8
Terry9022 <karta1020376@gmail.com>
2024-11-03 16:01:09 +0800

    Style Modifications

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   # get "home/index"
-  root "home#index"
+  # root "home#index"
   get "home/about"
+  root "friends#index"
 end
